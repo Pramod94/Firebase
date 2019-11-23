42d8dfef8b937904549dd9be3fd467c5a148e661
Pramod <pramodmithyantha@gmail.com>
2019-11-23 16:53:04 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <h3>FireBase</h3>
       </div>
+      </BrowserRouter>
     );
   }
 }
